66ce6432dfeb09c906de097ded4cf9a1bfc870ac
nicokrombach-hash <nico.krombach@googlemail.com>
2026-05-13 13:06:39 +0200

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://ntntxwzkizyppgjvolub.supabase.co',
+  'sb_publishable_bNSVQc5rYtCV9xWULO30GA_ueoHQSkl'
+)
